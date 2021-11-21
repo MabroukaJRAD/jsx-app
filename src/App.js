0d62e76f8b5b951d113b5edf16a851823bf328cb
Mabrouka JRAD <mabrouka.jrad@gmail.com>
2021-11-21 10:37:22 +0100

    updtates changes for public img

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import './Style.css';
+import imagesrc from './imagesrc.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+ <h1 className='title'>Your name here</h1>
+
+<br/>
+
+ <img src={imagesrc} alt='imageInsrc'/>
+
+ <br/>
+
+ <img src='/imageInPublic.png' alt='imageInPublic'/>
+
+</div>
+
+<video width="320" height="240" controls>
+
+ <source src='https://www.youtube.com/embed/ZaI2IlHwmgQ' type='video/mp4' />
+
+</video>
       </header>
     </div>
   );
